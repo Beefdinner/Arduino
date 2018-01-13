a5d581b5f0cd3fdeb996350d00a4e4462517324d
Beefdinner <jonathan.turner.m@hotmail.com>
2018-01-12 20:10:57 -0600

    Add files via upload

---
+++ PassiveBuzzer/PassiveBuzzer/PassiveBuzzer.ino
@@ -0,0 +1,21 @@
+#include "pitches.h"
+
+//da notes
+int melody[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5,NOTE_B5 NOTE_C6}
+int duration = 500; 
+
+
+void setup() {
+
+}
+
+void loop() {
+
+for (int thisNote = 0; thisNote<8; thisNote++)  {
+  {
+    tone(8, melody[thisNote], duration);
+    delay(1000);
+  }
+delay(2000);
+}
+
